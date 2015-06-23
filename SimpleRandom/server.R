aa1efa01bee6ab31337fba0f1c485990f04e8fb7
Homer White <homerhanumat@gmail.com>
2015-06-23 09:42:15 -0400

    better graphs in SimpleRandom

--- SimpleRandom/server.R
+++ SimpleRandom/server.R
@@ -1,37 +1,87 @@
 library(shiny)
-library(tigerstats,quietly=TRUE)
+library(scales)
+library(dplyr, quietly = TRUE)
+library(ggplot2, quietly = TRUE)
+library(tigerstats, quietly=TRUE)
 
 source("plot.R")
 
-shinyServer(function(input, output,session) {
+function(input, output) {
   
-  session$total <- 0
-  session$totalPrev <- 0
-  session$newVar <- TRUE
-  session$sample <- NULL
+  rv <- reactiveValues(
+    newVar = TRUE,
+    variable = "income",
+    factor = FALSE,
+    psData = NULL
+  )
+  
+  observeEvent(input$variable,
+               {
+                 rv$variable <- input$variable
+                 var <- imagpop[,rv$variable]
+                 if (is.factor(var)) {rv$factor <- TRUE
+                 } else rv$factor <- FALSE
+               })
 
-  update1 <- observe({
-    input$sample
-    if (input$sample > 0) {
-    session$newVar <- FALSE
-    print(session$newVar)
+  observeEvent(input$sample,
+              {
+              rv$newVar <- FALSE
+              pop <- imagpop[,rv$variable]
+              samp <- sample(pop, size = input$n, replace = FALSE)
+              popDF <- data.frame(values = pop, graph = rep("Population",length(pop)))
+              sampDF <- data.frame(values = samp, graph = rep("Sample",input$n))
+              rv$psData <- rbind(popDF, sampDF)
+              })
+  
+  
+  observeEvent(input$reset,
+          {
+          rv$newVar <- TRUE
+          rv$variable <- "income"
+          rv$factor <- FALSE
+          rv$psData <- NULL
+          })
+
+  output$newVar <- reactive({
+    rv$newVar
+  })
+  
+  outputOptions(output, "newVar", suspendWhenHidden = FALSE)
+  
+  output$initialPlot <- renderPlot({
+    if ( ! rv$factor ) {
+      ggplot(data = imagpop, aes_string(x = rv$variable), alpha = 0.5) +
+        geom_density(fill = "red") +
+        labs(title = "Density Plot of the Population")
+    } else {
+      ggplot(data = imagpop, aes_string(x = rv$variable)) +
+        geom_bar(fill = "red") +
+        labs(title = "Bar Graph of the Population")
     }
-  })
+    })
   
-  
-  update2 <- observe({
-    input$reset
-    session$newVar <- TRUE
-  })
-  
-  
-  
-  plotting <- eventReactive(input$sample,tryThis)
   output$plot <- renderPlot({
-    
-    if (input$sample > 0) {
-      form <- as.formula(paste0("~",input$variable))
-      print(histogram(form,data=imagpop))
+    if ( ! rv$newVar ) {
+      if ( ! rv$factor ) {
+        ggplot(data = rv$psData, aes(x = values, fill = graph)) +
+          geom_density(alpha = 0.5) +
+          labs(title = "Density Plot of the Population, with Sample",
+              x = rv$variable)
+      } else {
+        df <- rv$psData %>% group_by(graph, values) %>%
+          summarize(n = n()) %>%
+          mutate(relFreq = n/sum(n)) %>%
+          mutate(pos = 0.5 * relFreq) %>%
+          mutate(label = paste0(sprintf("%.1f", relFreq*100), "%"))
+        ggplot(data = df, aes(x = values, fill = graph)) +
+          geom_bar(aes(y = relFreq), stat = "identity", position = "dodge") +
+          scale_y_continuous(labels = percent) +
+          geom_text(aes(x = values, y = pos, label = label, ymax = relFreq),
+                    position = position_dodge(width = 1),size = 5) +
+          labs(title = "Density Plot of the Population, with Sample",
+              x = rv$variable)
+      }
     }
   })
-})
+  
+}
--- SimpleRandom/ui.R
+++ SimpleRandom/ui.R
@@ -1,34 +1,36 @@
 library(shiny)
 library(tigerstats)
 
-
 shinyUI(fluidPage(
-  
   titlePanel("Simple Random Sampling from Imagpop"),
-  
   sidebarLayout(
-    
-    sidebarPanel(width=3,
+    sidebarPanel(
+      width=3,
       conditionalPanel(
         condition="output.newVar == true",
         radioButtons("variable","Variable to study:",selected="income",
                      c(names(imagpop)))
         ),
-      numericInput("n","Desired Sample Size:",value=100,min=2,max=1000),
-      actionButton("sample",HTML("<strong>Take the Sample!</strong>")),
+      numericInput("n", "Desired Sample Size:", value=100, min=2, max=1000),
+      actionButton("sample", HTML("<strong>Take the Sample!</strong>")),
       conditionalPanel(
-        condition="session.newVar == false",
-        actionButton("reset",HTML("<strong>Start Over</strong>"))
+        condition="output.newVar == false",
+        actionButton("reset", HTML("<strong>Start Over</strong>"))
       )
     ),
-    
-    mainPanel(width=9,
-      
-      plotOutput("plot"),
-      tableOutput("table")
-      
-  )
+    mainPanel(
+      width=9,
+      conditionalPanel(
+        condition = "output.newVar == true",
+        plotOutput("initialPlot"),
+        tableOutput("initialTable")
+      ),
+      conditionalPanel(
+        condition = "output.newVar == false",
+        plotOutput("plot"),
+        tableOutput("table")
+        )
+      )
   
   )
-  
 ))
